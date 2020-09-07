b789ad10349f045ce42470a414ad5a8ba21599ef
kamniang <deborahoh94@gamil.com>
2020-09-07 21:16:23 +0900

    intent practice -exists error(about provider)

--- app/src/main/java/com/gyeong/practiceintent/MainActivity.java
+++ app/src/main/java/com/gyeong/practiceintent/MainActivity.java
@@ -3,6 +3,7 @@ package com.gyeong.practiceintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickContact(View view) {
+    }
+
+    public void clickCamera(View view) {
+    }
+
+    public void clickCamera2(View view) {
+    }
+
+    public void clickVoiceApp(View view) {
     }
 }
